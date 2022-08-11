4b20db019d3a824ad7f30fd23b3bcaf38d57aa41
Makarenko Ivan <ivan.makarenko@free.fr>
2022-08-11 19:18:23 +0200

    Validation formulaire

--- index.js
+++ index.js
@@ -29,3 +29,108 @@ for(i = 0; i < li.length; i++) {
     menu.classList.remove('responsive');
   })
 }
+
+// Déclaration du formulaire
+(function() { 
+  'use strict'
+  let form = document.getElementById('formulaire'); 
+  form.addEventListener('submit', function(event) {
+      
+      Array.from(form.elements).forEach((input) => { 
+          if (input.type !== "submit") { 
+              if (!validateFields(input)) { 
+                  
+                  event.preventDefault();
+                  event.stopPropagation();
+                  
+                  input.classList.remove("is-valid"); 
+                  input.classList.add("is-invalid");
+                  input.nextElementSibling.style.display = 'block';
+              } 
+              else {
+                  input.nextElementSibling.style.display = 'none';
+                  input.classList.remove("is-invalid"); 
+                  input.classList.add("is-valid"); 
+              }
+          }
+      });
+  }, false)
+})()
+
+
+
+
+
+
+// Création des fonctions de verefications
+
+// Validation d'un champ REQUIRED
+function validateRequired(input) {
+  return !(input.value == null || input.value == "");
+}
+
+// Validation des caractères : LATIN & LETTRES
+function validateText(input) {
+  return input.value.match("^[A-Za-z]+$");
+}
+
+// Validation du nombre de caractéres : MIN & MAX
+function validateLenght(input, minLength, maxLength) {
+    return !(input.value.length < minLength || input.value.length > maxLength);
+}
+
+// Validation d'un e-mail
+function validateEmail(input) {
+  let EMAIL = input.value;
+  let POSAT = EMAIL.indexOf("@");
+  let POSDOT = EMAIL.lastIndexOf(".");
+  return !(POSAT < 1 || (POSDOT - POSAT < 2));
+}
+
+
+
+
+// Validation des champs de formulaires
+
+function validateFields(input) {
+  let fieldName = input.name;
+  // Validaton de l'input PRENOM
+  if (fieldName == "firstname") {
+      if (!validateRequired(input)) {
+          return false;
+      }
+      if (!validateLenght(input, 2, 20)) {
+          return false;
+      }
+      if (!validateText(input)) {
+          return false;
+      }
+      return (true);
+  }
+
+  // Validaton de l'input MESSAGE
+  if (fieldName == "message") {
+    if (!validateRequired(input)) {
+        return false;
+    }
+    if (!validateLenght(input, 15, 1500)) {
+        return false;
+    }
+    if (!validateText(input)) {
+        return false;
+    }
+    return (true);
+  }
+
+  // Validaton de l'input EMAIL
+  if (fieldName == "email") {
+    if (!validateRequired(input)) {
+        return false;
+    }
+    if (!validateEmail(input)) {
+        return false;
+    }
+    return (true);
+  }
+}
+
