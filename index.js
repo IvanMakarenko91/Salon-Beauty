dc56b52a233a549b1d13a5a4851c0ec59afe9abe
Makarenko Ivan <ivan.makarenko@free.fr>
2022-08-11 18:42:08 +0200

    Fenetre modal au click

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const sec = document.querySelectorAll('section');
 
 function activeMenu() {
   let len = sec.length;
-  while(--len && window.scrollY + 97 < sec[len].offsetTop);
+  while(--len && window.scrollY + 72 < sec[len].offsetTop);
   li.forEach(ltx => ltx.classList.remove('activerr'));
   li[len].classList.add('activerr');
 }
